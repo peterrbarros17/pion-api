03d4cc4fc45f97db11efa8d24ec548d459a7659b
peterrbarros17 <peterrbarros17@gmail.com>
2024-06-28 15:59:13 -0300

    migrate project from js to ts

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import "dotenv/config.js";
 import app from "./src/app.js";
 
-const PORT = process.env.DB_CONNECTION_STRING || 3000;
+const PORT = process.env.PORT || process.env.DB_CONNECTION_STRING;
 
 app.listen(PORT, () => {
   console.log("Server online");
